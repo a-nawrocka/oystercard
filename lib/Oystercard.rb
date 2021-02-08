4063377b503d904dee66699fc36424f88ec064c6
Adam Jonas <adamjonas25@gmail.com>
2021-02-08 15:22:52 +0000

    define oystercard#deduct

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,16 +1,27 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :limit
+
+  DEFAULT_LIMIT = 90
 
   def initialize
     @balance = 0
+    @limit = DEFAULT_LIMIT
   end
 
   def top_up(amount)
     self.balance += amount
-    fail if amount + balance > 90
+    fail "#{amount} is over limit!" if over_limit?(amount)
+  end
+
+  def deduct(amount)
+    self.balance -= amount
   end
 
   private
 
   attr_writer :balance
+
+  def over_limit?(amount)
+    amount + balance > limit
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,7 @@
 require "Oystercard"
 describe Oystercard do
   context "when initialized" do
-    it "set balance od zero" do
+    it "set balance to zero" do
       expect(subject.balance).to eq 0
     end
   end
@@ -13,11 +13,21 @@ describe Oystercard do
         expect(subject.balance).to be 20
       end
     end
-    context 'when amount is more than maximum limit ' do 
-      it 'raises an error' do 
-        expect { (subject.top_up(91)) }.to raise_error 
-      end 
-    end  
+    context 'when amount is more than maximum limit ' do
+      it 'raises an error' do
+        expect { (subject.top_up(91)) }.to raise_error "91 is over limit!"
+      end
+    end
   end
 
+  describe '#deduct' do
+    before { subject.top_up(30) }
+
+    context 'when given argument 20' do
+      before { subject.deduct(20) }
+      it 'deducts 20 from balance' do
+        expect(subject.balance).to be 10
+      end
+    end
+  end
 end
